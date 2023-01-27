82183acf35ef32e9ebe845b28965f1666e2b649a
Nicolás Francisco López-ibarra Lúque <84680379+NMachinne@users.noreply.github.com>
2023-01-27 09:42:32 +0100

    Add files via upload

---
+++ src/app/admin/admin.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'admin.page.html',
+  styleUrls: ['admin.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+}
