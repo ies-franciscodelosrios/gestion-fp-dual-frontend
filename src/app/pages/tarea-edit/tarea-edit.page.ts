6ca0e4e484bffa368c5b3b2329953b3430f644c5
miguelllcas <miguelcasly@gmail.com>
2023-06-08 18:24:20 +0200

    tus muertos

--- src/app/pages/tarea-edit/tarea-edit.page.ts
+++ src/app/pages/tarea-edit/tarea-edit.page.ts
@@ -12,15 +12,18 @@ import {lastValueFrom} from 'rxjs'
 })
 
 export class TareaEditPage implements OnInit {
+  //tarea a modificar
   @Input() myEncargo: Encargo;
+  //formulario
   public formEncargo: FormGroup;
 
   constructor(private modalCTRL: ModalController,
     private formBuilder: FormBuilder,
     private apiS: APIService,
     ) { }
-
+     
   ngOnInit() {
+    //se actuliza los input con la data de la tarea
     this.formEncargo = this.formBuilder.group({
       comentario:`${this.myEncargo.comentario}`,
       estado:`${this.myEncargo.estado}`
@@ -31,8 +34,11 @@ export class TareaEditPage implements OnInit {
     this.modalCTRL.dismiss(null, 'cancel');
   }
 
+  //se guardan los cambios en la tarea
   async submitForm() {
+    //obtiene el valor del campo comentario 
       this.myEncargo.comentario=this.formEncargo.get('comentario')?.value;
+    //segun el estado de la tarea el icono de ella cambiara
       if(this.formEncargo.get('estado')?.value=="true"){
         this.myEncargo.estado=true;
       }else{
@@ -40,6 +46,7 @@ export class TareaEditPage implements OnInit {
       }
       
       try{
+        //actualiza la tarea
         const response = await lastValueFrom(this.apiS.updateEncargo({
           id: this.myEncargo.id,
           tarea:this.myEncargo.tarea,
